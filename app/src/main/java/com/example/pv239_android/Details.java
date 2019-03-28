06db06ba9a3e8bf984f7564d9a25a09762d04314
Tamas Rozsa <445653@mail.muni.cz>
2019-03-28 17:32:54 +0100

    details page started, jsut beta

---
+++ app/src/main/java/com/example/pv239_android/Details.java
@@ -0,0 +1,43 @@
+package com.example.pv239_android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.time.LocalDate;
+
+public class Details extends AppCompatActivity {
+
+    private String name;
+    private String description;
+    private LocalDate date;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.plan_details);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
