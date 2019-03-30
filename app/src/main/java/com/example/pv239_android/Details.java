366c8f7f44656988b874a613eaf1e9905ed6e02e
Tamas Rozsa <445653@mail.muni.cz>
2019-03-30 21:52:49 +0100

    pages created and buttons added

--- app/src/main/java/com/example/pv239_android/Details.java
+++ app/src/main/java/com/example/pv239_android/Details.java
@@ -1,12 +1,16 @@
 package com.example.pv239_android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
 
 import java.time.LocalDate;
 
 public class Details extends AppCompatActivity {
 
+    private static final String TAG = "Details";
     private String name;
     private String description;
     private LocalDate date;
@@ -15,6 +19,17 @@ public class Details extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.plan_details);
+
+        Log.d(TAG, "onCreate: Started");
+
+        Button saveButton = (Button) findViewById(R.id.btnSavePlan);
+        saveButton.setOnClickListener((v) -> {
+            Log.d(TAG, "onCreate: Save plan");
+
+            //TODO add saving the plan
+
+            startActivity(new Intent(Details.this, MainActivity.class));
+        });
     }
 
     public String getName() {
--- app/src/main/java/com/example/pv239_android/MainActivity.java
+++ app/src/main/java/com/example/pv239_android/MainActivity.java
@@ -3,23 +3,36 @@ package com.example.pv239_android;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button createNew = (Button) findViewById(R.id.create_new_plan);
-        createNew.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Details.class));
-            }
+        Log.d(TAG, "onCreate: Started");
+        
+        Button createNewBtn = (Button) findViewById(R.id.btnCreateNewPlan);
+        createNewBtn.setOnClickListener(v -> {
+            Log.d(TAG, "onClick: Crate new plan");
+            startActivity(new Intent(MainActivity.this, Details.class));
+        });
+
+        Button calendarBtn = (Button) findViewById(R.id.btnCalendar);
+        calendarBtn.setOnClickListener((v) -> {
+            Log.d(TAG, "onCreate: Calendar");
+            startActivity(new Intent(MainActivity.this, Calendar.class));
+        });
+
+        Button historyBtn = (Button) findViewById(R.id.btnHistory);
+        historyBtn.setOnClickListener((v) -> {
+            Log.d(TAG, "onCreate: History");
+            startActivity(new Intent(MainActivity.this, Calendar.class));
         });
     }
 }
